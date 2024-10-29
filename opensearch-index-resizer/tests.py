272a7406e5092a2bbf3dae42fb12a7b04e5c310e
Aditya Purandare <aditya.p1993@hotmail.com>
2024-10-29 22:07:03 +0530

    Create tests.py

---
+++ opensearch-index-resizer/tests.py
@@ -0,0 +1,23 @@
+import unittest
+import application
+
+class ApplicationTests(unittest.TestCase):
+
+    def setUp(self):
+        self.es_client = application.connect_elasticsearch(
+                        host='elasticsearch.uacf.io',
+                        scheme='https',
+                        port=443)
+
+    def tearDown(self):
+        pass
+
+    def test_connect_elasticsearch(self):
+        self.assertIsNotNone(self.es_client)
+
+    def test_get_all_indices(self):
+        indices_list = application.get_all_indices(self.es_client)
+        self.assertIsNotNone(indices_list[0]['index'])
+        self.assertIsNotNone(indices_list[0]['docs.count'])
+        self.assertIsNotNone(indices_list[0]['store.size'])
+        self.assertTrue(len(indices_list)!=0)
